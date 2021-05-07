73da0ffd26efecc1fce34fc2f904b627b98ee38e
Dasha <dariademidova7@gmail.com>
2021-05-07 12:07:49 +0300

    Конструктор и инкапсуляция

---
+++ Snake/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    { /*хранение данных о точке*/
+        public int x;
+        public int y;
+        public char sym;
+
+        public Point()
+        {
+        }
+
+        public Point(int _x, int _y, char _sym) /*конструктор, принимающий значения и затем передающий их в хранящиеся данные*/
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw() /*вывод данных на экран*/
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);         
+        }
+    }
+}
